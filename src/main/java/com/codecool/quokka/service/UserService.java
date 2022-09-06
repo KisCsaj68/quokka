d635f7c4e4efc823bedea8f2c4e636aca3201679
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-09-06 15:38:36 +0200

    ADD: get user by id method

--- src/main/java/com/codecool/quokka/service/UserService.java
+++ src/main/java/com/codecool/quokka/service/UserService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -25,5 +27,9 @@ public class UserService {
 
     public Set<UserDto> getAllUser() {
         return userDao.getAllUser();
+    }
+
+    public Optional<UserDto> getUser(UUID id) {
+        return userDao.getUser(id);
     }
 }
