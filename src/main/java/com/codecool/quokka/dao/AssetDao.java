1408f41c2642e12e679250f059a70b3e926335e7
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-09-07 12:01:02 +0200

    ADD: crypto and stock name getter

--- src/main/java/com/codecool/quokka/dao/AssetDao.java
+++ src/main/java/com/codecool/quokka/dao/AssetDao.java
@@ -1,16 +1,18 @@
 package com.codecool.quokka.dao;
 
-import com.codecool.quokka.service.Stock;
+import com.codecool.quokka.service.Asset;
 
 import java.util.Set;
 
-public interface StockDao {
-    Stock add(Stock stock);
+public interface AssetDao {
+    Asset add(Asset asset);
 
     Set<String> getAll();
+    Set<String> getAllStock();
+    Set<String> getAllCrypto();
 
-    Stock get(int id);
+    Asset get(int id);
 
-    Stock get(String symbol);
+    Asset get(String symbol);
 
 }
