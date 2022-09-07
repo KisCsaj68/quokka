6436a3f0290bdeff791ea1ebcb0d4dc20528ad6b
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-09-07 11:25:18 +0200

    FIX: update user method

--- src/main/java/com/codecool/quokka/dao/implementation/UserDaoMem.java
+++ src/main/java/com/codecool/quokka/dao/implementation/UserDaoMem.java
@@ -5,21 +5,19 @@ import com.codecool.quokka.model.User;
 import com.codecool.quokka.model.UserDto;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Repository("inMemoUserDao")
 public class UserDaoMem implements UserDao {
 
     private static Set<User> DB = new HashSet<>();
+
     @Override
     public UserDto addUser(String name, String userName, String emailAddress, String passWord) {
         User newUser = new User(name, emailAddress, userName, passWord);
         DB.add(newUser);
-        return new UserDto(userName,newUser.getId(), name, emailAddress);
+        return new UserDto(userName, newUser.getId(), name, emailAddress);
     }
 
     @Override
@@ -40,7 +38,7 @@ public class UserDaoMem implements UserDao {
         Optional<UserDto> user = DB.stream()
                 .filter(e -> e.getId().equals(id))
                 .findFirst()
-                .map(e -> new UserDto(e.getUserName(),e.getId(), e.getFullName(), e.getEmailAddress()));
+                .map(e -> new UserDto(e.getUserName(), e.getId(), e.getFullName(), e.getEmailAddress()));
         return user;
     }
 
@@ -52,5 +50,30 @@ public class UserDaoMem implements UserDao {
         if (user.isPresent()) {
             DB.remove(user.get());
         }
+    }
+
+    @Override
+    public Optional<UserDto> updateUser(UUID id, HashMap<String, String> fields) {
+        Optional<User> user = DB.stream()
+                .filter(e -> e.getId().equals(id))
+                .findFirst();
+        if (user.isPresent()) {
+            User actualUser = user.get();
+            for (String item : fields.keySet()) {
+                switch (item) {
+                    case "emailAddress":
+                        actualUser.setEmailAddress(fields.get("emailAddress"));
+                        break;
+                    case "fullName":
+                        actualUser.setFullName(fields.get("fullName"));
+                    case "passWord":
+                        actualUser.setPassWord(fields.get("passWord"));
+                }
+            }
+            Optional<UserDto> actualDto = Optional.of(new UserDto(actualUser.getUserName(), actualUser.getId(),
+                                                        actualUser.getFullName(), actualUser.getEmailAddress()));
+            return actualDto;
+        }
+        return Optional.empty();
     }
 }
