92f358a7c24d8e864d220ef6090a5efc87caf2af
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-09-07 12:04:37 +0200

    ADD: crpyto&&stock setter/getter

--- src/main/java/com/codecool/quokka/service/AssetService.java
+++ src/main/java/com/codecool/quokka/service/AssetService.java
@@ -1,24 +1,51 @@
 package com.codecool.quokka.service;
 
-import com.codecool.quokka.dao.StockDao;
+import com.codecool.quokka.dao.AssetDao;
+import com.codecool.quokka.model.AssetType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.Set;
 
 @Service
-public class StockService {
-    private final StockDao stockDao;
+public class AssetService {
+    private final AssetDao assetDao;
 
     @Autowired
-    public StockService(StockDao stockDao) {
-        this.stockDao = stockDao;
+    public AssetService(AssetDao assetDao) {
+        this.assetDao = assetDao;
     }
 
-    public StockDto addStock(Stock stock) {
-        Stock newStock = this.stockDao.add(stock);
-        String symbol = newStock.getSymbol();
-        BigDecimal price = newStock.getPrice();
-        return new StockDto(symbol, price);
+    private AssetDto createDto(Asset asset) {
+        String symbol = asset.getSymbol();
+        BigDecimal price = asset.getPrice();
+        AssetType type = asset.getType();
+        return new AssetDto(symbol, price, type);
+    }
+
+    public AssetDto addAssetStock(Asset asset) {
+        asset.setType(AssetType.STOCK);
+        Asset newAsset = this.assetDao.add(asset);
+        System.out.println(newAsset);
+        return this.createDto(newAsset);
+    }
+
+    public AssetDto addAssetCrypto(Asset asset) {
+        asset.setType(AssetType.STOCK);
+        Asset newAsset = this.assetDao.add(asset);
+        return this.createDto(newAsset);
+    }
+
+    public Set<String> getAllAsset() {
+        return this.assetDao.getAll();
+    }
+
+    public Set<String> getAllStock() {
+        return this.assetDao.getAllStock();
+    }
+
+    public Set<String> getAllCrypto() {
+        return this.assetDao.getAllCrypto();
     }
 }
