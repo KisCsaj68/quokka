0b5844cd2a9093be25d387ebe149384c9cc1fd0c
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-09-07 12:00:22 +0200

    ADD: crypto&stock as Asset thus refact oldStockMethods

--- src/main/java/com/codecool/quokka/controller/AssetController.java
+++ src/main/java/com/codecool/quokka/controller/AssetController.java
@@ -1,23 +1,45 @@
 package com.codecool.quokka.controller;
 
-import com.codecool.quokka.service.Stock;
-import com.codecool.quokka.service.StockDto;
-import com.codecool.quokka.service.StockService;
+import com.codecool.quokka.service.Asset;
+import com.codecool.quokka.service.AssetDto;
+import com.codecool.quokka.service.AssetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/v1/stock")
+import java.util.Set;
+
+@RequestMapping("/api/v1/asset")
 @RestController
-public class StockController {
-    private final StockService stockService;
+public class AssetController {
+    private final AssetService assetService;
 
     @Autowired
-    public StockController(StockService stockService) {
-        this.stockService = stockService;
+    public AssetController(AssetService assetService) {
+        this.assetService = assetService;
     }
 
-    @PostMapping
-    public @ResponseBody StockDto addStock(@RequestBody Stock stock) {
-        return this.stockService.addStock(stock);
+    @PostMapping(path = "stock")
+    public @ResponseBody AssetDto addAssetStock(@RequestBody Asset asset) {
+        return this.assetService.addAssetStock(asset);
+    }
+
+    @PostMapping(path = "crypto")
+    public @ResponseBody AssetDto addAssetCrypto(@RequestBody Asset asset) {
+        return this.assetService.addAssetCrypto(asset);
+    }
+
+    @GetMapping
+    public Set<String> getAllAsset() {
+        return this.assetService.getAllAsset();
+    }
+
+    @GetMapping(path = "stock")
+    public Set<String> getAllStock() {
+        return this.assetService.getAllStock();
+    }
+
+    @GetMapping(path = "crypto")
+    public Set<String> getAllCrypto() {
+        return this.assetService.getAllCrypto();
     }
 }
