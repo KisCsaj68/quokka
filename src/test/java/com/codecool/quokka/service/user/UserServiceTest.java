a08b08859d2178177d3de11d10fb0ff578c1f5ec
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-09-08 17:48:00 +0200

    UPDATE: pom.xml

--- src/test/java/com/codecool/quokka/service/user/UserServiceTest.java
+++ src/test/java/com/codecool/quokka/service/user/UserServiceTest.java
@@ -1,37 +1,43 @@
 package com.codecool.quokka.service;
 
-import com.codecool.quokka.dao.implementation.UserDaoMem;
-import com.codecool.quokka.model.User;
-import com.codecool.quokka.model.UserDto;
+import com.codecool.quokka.dao.user.UserDao;
+import com.codecool.quokka.dao.user.implementation.UserDaoMem;
+import com.codecool.quokka.model.user.User;
+import com.codecool.quokka.model.user.UserDto;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.HashMap;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
 @SpringBootTest
 @ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 class UserServiceTest {
     static User user;
     static UserDto dto;
+    static UserDto updatedDto;
     static UUID id;
     static UUID fakeId;
+    static HashMap<String, String> data;
 
     @Autowired
     private UserService userService;
 
     @Autowired
-    private UserDaoMem userDaoMem;
+    private UserDao userDao;
 
     @BeforeAll
     public static void setUp() {
@@ -39,27 +45,30 @@ class UserServiceTest {
          fakeId = UUID.fromString("b462290f-4008-4d71-8a39-e956e245ede8");
          user = new User("Test User", "test@asd.com", "TestUser", "asd", id);
          dto = new UserDto(user.getUserName(), user.getId(), user.getFullName(), user.getEmailAddress());
+         updatedDto = new UserDto("User3456", user.getId(), user.getFullName(), user.getEmailAddress());
+         data = new HashMap<>();
+         data.put("fullName", "User3456");
     }
 
 
     @Test
     void addUser() {
-        Mockito.when(userDaoMem.addUser(user)).thenReturn(dto);
+        Mockito.when(userDao.addUser(user)).thenReturn(dto);
         UserDto resultDto = userService.addUser(user);
         assertEquals(dto,resultDto);
     }
 
     @Test
     void getAllUser() {
-        Mockito.when(userDaoMem.getAllUser()).thenReturn(Set.of(dto));
+        Mockito.when(userDao.getAllUser()).thenReturn(Set.of(dto));
         Set<UserDto> dtos = userService.getAllUser();
         assertEquals(dtos, Set.of(dto));
     }
 
     @Test
     void getUser() {
-        Mockito.when(userDaoMem.getUser(id)).thenReturn(Optional.of(dto));
-        Mockito.when(userDaoMem.getUser(fakeId)).thenReturn(Optional.empty());
+        Mockito.when(userDao.getUser(id)).thenReturn(Optional.of(dto));
+        Mockito.when(userDao.getUser(fakeId)).thenReturn(Optional.empty());
         Optional<UserDto> resultDto = userService.getUser(id);
         Optional<UserDto> fakeDto = userService.getUser(fakeId);
         assertEquals(resultDto, Optional.of(dto));
@@ -67,12 +76,20 @@ class UserServiceTest {
     }
 
     @Test
-    void deleteUser() {
-
+    void updateUser() {
+        Mockito.when(userDao.updateUser(id, data)).thenReturn(Optional.of(updatedDto));
+        Mockito.when(userDao.updateUser(fakeId, data)).thenReturn(Optional.empty());
+        Optional<UserDto> resultDto = userService.updateUser(id,data);
+        Optional<UserDto> fakeResultDto = userService.updateUser(fakeId,data);
+        assertEquals(resultDto, Optional.of(dto));
+        assertEquals(fakeResultDto, Optional.empty());
     }
 
     @Test
-    void updateUser() {
+    void deleteUser1() {
+        userService.deleteUser(id);
+        Mockito.verify(userDao, Mockito.times(1)).deleteUser(any());
+
     }
 
 
