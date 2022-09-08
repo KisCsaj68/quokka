4bc00d19835568c22f6440a5607c6073923dde97
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-09-08 09:27:10 +0200

    CREATE: utils folder with utils class

---
+++ src/main/java/com/codecool/quokka/utils/Utils.java
@@ -0,0 +1,25 @@
+package com.codecool.quokka.utils;
+
+import com.codecool.quokka.model.AssetType;
+
+public class Utils {
+
+    public static int numParser(String string) {
+        if (string == null || string.equals("")) {
+            return -1;
+        }
+
+        try {
+            return Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+    public static AssetType assetTypeParser(String assetType) {
+        try {
+            return AssetType.valueOf(assetType.toUpperCase());
+        } catch (IllegalArgumentException i) {
+            return null;
+        }
+    }
+}
