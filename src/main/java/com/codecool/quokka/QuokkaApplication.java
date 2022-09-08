f55b5849d3fd8829fcdd995c75e1fca0f12334a9
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-09-08 13:41:04 +0200

    ADD: applicationContext

--- src/main/java/com/codecool/quokka/QuokkaApplication.java
+++ src/main/java/com/codecool/quokka/QuokkaApplication.java
@@ -2,11 +2,15 @@ package com.codecool.quokka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class QuokkaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuokkaApplication.class, args);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
     }
+
 }
