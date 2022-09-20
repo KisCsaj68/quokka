762faf9e4387af8516f5a630c67024c50354ee94
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-09-20 14:35:22 +0200

    FIX: basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,76 @@
 import NavigationBar from "./NavigationBar"
 import SideBar from "./SideBar"
+import {Link, Route, Routes, useNavigate} from "react-router-dom";
+import Home from "./Home";
+import Registration from "./Registration"
+import Stock from "./Stock";
+import LogIn from "./LogIn";
+import Crypto from "./Crypto";
+import {useState} from "react";
+import apiRequest from "./apiRequest";
 
 function App() {
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
+    const [passWrd, setPassWrd] = useState('');
+    const history = useNavigate();
+
+    const handleRegistration = async (e) => {
+        e.preventDefault();
+        const newUser = {fullName, email, userName, passWrd}
+        try {
+            const response = await apiRequest.post('/api/v1/user', newUser);
+            history('/login');
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+        }
+        setEmail("");
+        setPassWrd("");
+        setFullName("");
+        setEmail("");
+        setUserName("");
+    }
+
+    const addNewUser = async (fullName, email, userName, passWrd) => {
+        const newUser = {fullName, email, userName, passWrd}
+        const optionPost = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newUser)
+        };
+        const response = await apiRequest("/api/v1/user", optionPost, null);
+
+
+    }
     return (
         <div className="App">
             <NavigationBar/>
             <SideBar/>
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/registration"
+                           element={<Registration
+                                        handleReg={handleRegistration}
+                                        fullName={fullName}
+                                        setFullName={setFullName}
+                                        userName={userName}
+                                        setUserName={setUserName}
+                                        email={email}
+                                        setEmail={setEmail}
+                                        passWrd={[passWrd]}
+                                        setPassWrd={setPassWrd}
+                                    />}
+                    />
+                    <Route path="/login" element={<LogIn />} />
+                    <Route path="/stock" element={<Stock />} />
+                    <Route path="/crypto" element={<Crypto />} />
+                </Routes>
+            </div>
+
         </div>
     );
 }
