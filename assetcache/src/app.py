323d0add58ffe9cb86ed7fd1eb1073d514115e20
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-09-21 14:22:42 +0200

    ADD: main APP class

---
+++ assetcache/src/app.py
@@ -0,0 +1,46 @@
+import falcon
+
+from src.cache import CryptoStore
+from src.cache import StockStore
+from src.routes import AllAssetRoute, Ping, AssetRoute
+
+
+class AssetCacheAPI:
+    def __init__(self) -> None:
+        self._app: falcon.App = falcon.App()
+        self.crypto_route = None
+        self.stock_route = None
+        self.ping = None
+        self.all_asset_route = None
+
+        self.create_fields()
+        self.add_routes()
+
+    @property
+    def app(self) -> falcon.App:
+        return self._app
+
+    def create_fields(self) -> None:
+        crypto_store: CryptoStore = CryptoStore()
+        stock_store: StockStore = StockStore()
+        self.all_asset_route: AllAssetRoute = AllAssetRoute(
+            (crypto_store, stock_store)
+        )
+        self.stock_route: AssetRoute = AssetRoute(stock_store)
+        self.crypto_route: AssetRoute = AssetRoute(crypto_store)
+        self.ping: Ping = Ping()
+
+    def add_routes(self) -> None:
+        self._app.add_route("/ping", self.ping)
+        self._app.add_route("/api/v1/asset", self.all_asset_route)
+        self._app.add_route("/api/v1/stock", self.stock_route)
+        self._app.add_route("/api/v1/stock/{name}", self.stock_route,
+                            suffix="asset")
+        self._app.add_route("/api/v1/crypto", self.crypto_route)
+        self._app.add_route("/api/v1/crypto/{name}", self.crypto_route,
+                            suffix="asset")
+
+
+def run() -> falcon.App:
+    cache: AssetCacheAPI = AssetCacheAPI()
+    return cache.app
