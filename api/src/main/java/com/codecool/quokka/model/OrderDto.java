89cd4167cb23206d9728fd0384ae01b7788d9d5f
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-10-03 18:28:46 +0200

    ADD: OrderDto

---
+++ api/src/main/java/com/codecool/quokka/model/OrderDto.java
@@ -0,0 +1,42 @@
+package com.codecool.quokka.model;
+
+import com.codecool.quokka.model.order.Order;
+import com.codecool.quokka.model.order.OrderStatus;
+import com.codecool.quokka.model.order.OrderType;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class OrderDto {
+    private int quantity;
+    private String symbol;
+    private OrderType type;
+    private BigDecimal limit;
+
+    public OrderDto(int qty, String symbol, OrderType type, BigDecimal limit) {
+        this.quantity = qty;
+        this.symbol = symbol;
+        this.type = type;
+        this.limit = limit;
+    }
+
+    public int getQty() {
+        return quantity;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public OrderType getType() {
+        return type;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public Order toEntity(UUID accountId) {
+        return new Order(this.getQty(), null, accountId, OrderStatus.OPEN, this.getType(), this.getLimit());
+    }
+}
