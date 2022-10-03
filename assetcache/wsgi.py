ed46b6d3f92b3891e42dbca0eb73d4c3fa6fa39b
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-10-04 01:21:42 +0200

    ADD: env storage and primitive db

--- assetcache/wsgi.py
+++ assetcache/wsgi.py
@@ -1,9 +1,15 @@
-from os.path import join, dirname
-from dotenv import load_dotenv
+from typing import Tuple
+
 from src import AssetCacheAPI
+from src.data_handlers import DataCollectors
+from src.storages import DotEnvConfig
+from src.storages import PrimitiveJsonDB
 
-dotenv_path: str = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+ENV_VARS: Tuple[str, ...] = ("APCA_API_KEY_ID", "APCA_API_SECRET_KEY",
+                             "APCA_API_BASE_URL", "APCA_API_VERSION")
+conf: DotEnvConfig = DotEnvConfig(ENV_VARS)
+dummy_db: PrimitiveJsonDB = PrimitiveJsonDB()
 
-asset_cache_api: AssetCacheAPI = AssetCacheAPI()
+data_collectors: DataCollectors = DataCollectors(dummy_db, conf)
+asset_cache_api: AssetCacheAPI = AssetCacheAPI(dummy_db, data_collectors)
 application = asset_cache_api.app
