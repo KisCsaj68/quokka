84775e178ee8c177548f8161e57864b9c7f3ae3f
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-10-04 14:33:18 +0200

    ADD: OrderService

--- oms/src/main/java/com/codecool/quokka/oms/controller/OrderController.java
+++ oms/src/main/java/com/codecool/quokka/oms/controller/OrderController.java
@@ -1,6 +1,9 @@
 package com.codecool.quokka.oms.controller;
 
-import com.codecool.quokka.model.order.Order;
+import com.codecool.quokka.model.order.AssetOrder;
+import com.codecool.quokka.model.order.OrderType;
+import com.codecool.quokka.oms.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,19 +11,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-
 @RestController
 @RequestMapping("/api/v1/order")
 public class OrderController {
+    private OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     @PostMapping
-    public ResponseEntity createOrder(@RequestBody Order data) {
-        // Send open order to persister via RMQ
-        // Ask the actual price from assetcache
-        // Fill the price to the order and update the order in DB.
-        // Create position, send to persister RMQ
-        // Store both Entity in-memory
+    public ResponseEntity createOrder(@RequestBody AssetOrder data) {
+        if (data.getType().equals(OrderType.LIMIT)){
+            return new ResponseEntity<>("Limit order not supported yet", HttpStatus.BAD_REQUEST);
+        }
         System.out.println("Hello from OMS" + data);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
--- oms/src/main/java/com/codecool/quokka/oms/service/OrderService.java
+++ oms/src/main/java/com/codecool/quokka/oms/service/OrderService.java
@@ -1,4 +1,13 @@
 package com.codecool.quokka.oms.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+
+    // Send open order to persister via RMQ
+    // Ask the actual price from assetcache
+    // Fill the price to the order and update the order in DB.
+    // Create position, send to persister RMQ
+    // Store both Entity in-memory
 }
