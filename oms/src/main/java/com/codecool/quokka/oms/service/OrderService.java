2e4ea3a2753ab902de2d6e2fff8171f5d6a41f37
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-10-05 15:08:44 +0200

    ADD: OrderService with RMQ

--- oms/src/main/java/com/codecool/quokka/oms/service/OrderService.java
+++ oms/src/main/java/com/codecool/quokka/oms/service/OrderService.java
@@ -1,13 +1,39 @@
 package com.codecool.quokka.oms.service;
 
+import com.codecool.quokka.model.order.AssetOrder;
+import com.codecool.quokka.model.order.OrderStatus;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+@Component
 public class OrderService {
 
-    // Send open order to persister via RMQ
-    // Ask the actual price from assetcache
-    // Fill the price to the order and update the order in DB.
-    // Create position, send to persister RMQ
-    // Store both Entity in-memory
+    private RabbitTemplate template;
+
+    @Autowired
+    public OrderService(RabbitTemplate template) {
+        this.template = template;
+    }
+
+    public ResponseEntity createOrder(AssetOrder data) {
+        data.setStatus(OrderStatus.OPEN);
+        System.out.println(data);
+
+        // Send open order to persister via RMQ
+        template.convertAndSend(MQConfig.EXCHANGE, MQConfig.ROUTING_KEY, data);
+        System.out.println("Order received");
+
+
+        // Ask the actual price from assetcache port 8000.
+
+        // Fill the price to the order and update the order in DB.
+        // Create position, send to persister RMQ
+        // Store both Entity in-memory
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
 }
