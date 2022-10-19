348ae6f6ebe20d8bb843809d6a181d9ce3daaa8a
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-10-19 16:37:22 +0200

    FIX: oms fat-jar

--- oms/src/main/java/com/codecool/quokka/oms/OrderManagementService.java
+++ oms/src/main/java/com/codecool/quokka/oms/OrderManagementService.java
@@ -2,8 +2,10 @@ package com.codecool.quokka.oms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.codecool.quokka.model")
 public class OrderManagementService {
     public static void main(String[] args) {
         SpringApplication.run(OrderManagementService.class, args);
