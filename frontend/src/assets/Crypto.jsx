b9ce99aeb1c5204e682ce8b804cec802f865099f
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-10-20 22:40:49 +0200

    MOVE: Components to their folder

---
+++ frontend/src/assets/Crypto.jsx
@@ -0,0 +1,9 @@
+import Data from "./table/Data";
+
+const Crypto = () => {
+
+    return <Data type={"crypto"}/>
+
+}
+
+export default Crypto;
---
+++ frontend/src/assets/Stock.jsx
@@ -0,0 +1,7 @@
+import Data from "./table/Data";
+
+const Stock = () => {
+    return <Data type={"stock"}/>
+}
+
+export default Stock;
