09301696dfeab77c1c2be549ba6872d72aa02b22
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-11-03 13:39:37 +0100

    FIX: password hashing while update

--- api/src/main/java/com/codecool/quokka/service/account/AccountService.java
+++ api/src/main/java/com/codecool/quokka/service/account/AccountService.java
@@ -3,10 +3,12 @@ package com.codecool.quokka.service.account;
 import com.codecool.quokka.model.account.Account;
 import com.codecool.quokka.dao.account.AccountDao;
 import com.codecool.quokka.model.account.AccountDto;
+import com.google.common.hash.Hashing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -19,8 +21,7 @@ import java.util.stream.StreamSupport;
 @Service
 public class AccountService {
 
-    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
-            Pattern.compile("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", Pattern.CASE_INSENSITIVE);
 
     private final AccountDao accountDao;
 
@@ -37,9 +38,7 @@ public class AccountService {
     }
 
     public Set<AccountDto> getAllAccount() {
-        return StreamSupport.stream(accountDao.findAll().spliterator(), false)
-                .map(AccountDto::from)
-                .collect(Collectors.toSet());
+        return StreamSupport.stream(accountDao.findAll().spliterator(), false).map(AccountDto::from).collect(Collectors.toSet());
     }
 
     public Optional<AccountDto> getAccount(UUID id) {
@@ -71,7 +70,8 @@ public class AccountService {
                     acc.setFullName(fields.get("fullName"));
                     break;
                 case "password":
-                    acc.setPassword(fields.get("password"));
+                    String password = Hashing.sha256().hashString(fields.get("password"), StandardCharsets.UTF_8).toString();
+                    acc.setPassword(password);
                     break;
             }
         }
