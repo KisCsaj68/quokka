ad712ed7a4e59d0c062c0dadc5d95b3b2092c0fa
Viki <34596151+KisCsaj68@users.noreply.github.com>
2022-11-21 10:41:22 +0100

    FIX: move hardcoded address to application.properties (#83)

--- api/src/main/java/com/codecool/quokka/controller/FinController.java
+++ api/src/main/java/com/codecool/quokka/controller/FinController.java
@@ -3,6 +3,8 @@ package com.codecool.quokka.controller;
 import com.codecool.quokka.model.OrderDto;
 import com.codecool.quokka.model.assets.AssetType;
 import com.codecool.quokka.model.order.Orders;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +17,13 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/order")
+@ConfigurationProperties
 public class FinController {
     public static final UUID ACCOUNT_ID = UUID.fromString("a1521309-f533-460a-a9fc-3028b0efc79b");
     private RestTemplate restTemplate = new RestTemplate();
-    private String url = "http://oms:9000/api/v1/order";
+
+    @Value("${quokka.service.oms.address}${quokka.service.oms.endpoint}")
+    private String url;
 
     @PostMapping(path = "stock")
     public ResponseEntity createNewStockOrder(@RequestBody OrderDto data) {
