9d64d4b2cb6348aa9192430beb171899bc4d72cf
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-11-21 13:17:53 +0100

    ADD: common mqconfig file

---
+++ common/src/main/java/com/codecool/quokka/model/mqconfig/Config.java
@@ -0,0 +1,45 @@
+package com.codecool.quokka.model.mqconfig;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+
+public class Config {
+
+    public static final String ORDER_QUEUE = "order_queue";
+    public static final String POSITION_QUEUE = "position_queue";
+    public static final String EXCHANGE = "order_exchange";
+    public static final String ORDER_ROUTING_KEY = "order_routingKey";
+    public static final String POSITION_ROUTING_KEY = "position_routingKey";
+
+    public static TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    public static Declarables topicBindings(TopicExchange topicExchange) {
+        Queue orderQueue = new Queue(ORDER_QUEUE);
+        Queue positionQueue = new Queue(POSITION_QUEUE);
+        return new Declarables(
+                orderQueue,
+                positionQueue,
+                topicExchange,
+                BindingBuilder
+                        .bind(orderQueue)
+                        .to(topicExchange).with(ORDER_ROUTING_KEY),
+                BindingBuilder
+                        .bind(positionQueue)
+                        .to(topicExchange).with(POSITION_ROUTING_KEY));
+    }
+
+    public static MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    public static AmqpTemplate template(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+}
