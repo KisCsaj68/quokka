ce1ab147d5be15cabb76cad1dfe2100e1a9d955b
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-11-22 14:35:26 +0100

    UPDATE: account class with UserSetails implementation

--- common/src/main/java/com/codecool/quokka/model/account/Account.java
+++ common/src/main/java/com/codecool/quokka/model/account/Account.java
@@ -1,17 +1,22 @@
 package com.codecool.quokka.model.account;
 
+import com.codecool.quokka.model.role.AccountRole;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.hash.Hashing;
+import lombok.AllArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 @Entity
-public class Account {
+@AllArgsConstructor
+public class Account implements UserDetails {
 
     @Id
     @GeneratedValue(generator = "uuid")
@@ -24,23 +29,46 @@ public class Account {
     private String emailAddress;
 
     private String password;
+    private final boolean isAccountNonExpired;
+    private final boolean isAccountNonLocked;
+    private final boolean isCredentialsNonExpired;
+    private final boolean isEnabled;
+    @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+    private final Set<AccountRole> roles;
 
-    public Account(@JsonProperty("full_name") String fullName, @JsonProperty("email_address") String emailAddress, @JsonProperty("user_name") String userName, @JsonProperty("password") String password) {
+    public Account(@JsonProperty("full_name") String fullName, @JsonProperty("email_address") String emailAddress, @JsonProperty("user_name") String userName, @JsonProperty("password") String password, boolean isAccountNonExpired, boolean isAccountNonLocked, boolean isCredentialsNonExpired, boolean isEnabled, Set<AccountRole> roles) {
         this.fullName = fullName;
         this.userName = userName;
         this.emailAddress = emailAddress;
         this.password = password;
+        this.roles = roles;
+        this.isAccountNonExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+        this.isEnabled = isEnabled;
+
     }
 
-    public Account(String fullName, String emailAddress, String userName, String password, UUID id) {
+    public Account(String fullName, String emailAddress, String userName, String password, UUID id, boolean isAccountNonExpired, boolean isAccountNonLocked, boolean isCredentialsNonExpired, boolean isEnabled, Set<AccountRole> roles) {
         this.id = id;
         this.fullName = fullName;
         this.userName = userName;
         this.emailAddress = emailAddress;
         this.password = password;
+        this.roles = roles;
+        this.isAccountNonExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+        this.isEnabled = isEnabled;
     }
 
     public Account() {
+        this.roles = new HashSet<>();
+        this.isAccountNonExpired = true;
+        this.isAccountNonLocked = true;
+        this.isCredentialsNonExpired = true;
+        this.isEnabled = true;
+
     }
 
     public UUID getId() {
@@ -59,9 +87,11 @@ public class Account {
         return emailAddress;
     }
 
-    public String getPassword() {
-        return password;
-    }
+
+//    public String getPassword() {
+//        return password;
+//    }
+
 
     public void setFullName(String name) {
         this.fullName = name;
@@ -103,6 +133,45 @@ public class Account {
     @Override
     public String toString() {
         return "Account{" + "id=" + id + ", fullName='" + fullName + '\'' + ", userName='" + userName + '\'' + ", emailAddress='" + emailAddress + '\'' + ", password='" + password + '\'' + '}';
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authList = new HashSet<>();
+        for (AccountRole role : this.roles) {
+            authList.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authList;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
     }
 }
 
