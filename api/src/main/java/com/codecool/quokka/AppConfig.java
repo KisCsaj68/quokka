6adfdc932915585f44e47b46873dcaf7e1bef29e
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-11-24 15:35:27 +0100

    FIX: Beans

--- api/src/main/java/com/codecool/quokka/AppConfig.java
+++ api/src/main/java/com/codecool/quokka/AppConfig.java
@@ -29,13 +29,4 @@ public class AppConfig {
     public JwtConfig getJwtConfig() {
         return new JwtConfig();
     }
-
-
-
-
-
-    @Bean
-    public TokenEncoder getTokenEncoder() {
-        return new TokenEncoder(getJwtConfig());
-    }
 }
