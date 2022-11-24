2b5fce44ecccc7484516176227792c152e1c075b
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-11-24 15:37:53 +0100

    ADD: AccountRoleDao to able to get persisted roles

---
+++ api/src/main/java/com/codecool/quokka/dao/AccountRoleDao.java
@@ -0,0 +1,10 @@
+package com.codecool.quokka.dao;
+
+import com.codecool.quokka.model.role.AccountRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRoleDao extends JpaRepository<AccountRole, Long> {
+    Optional<AccountRole> getAccountRoleByName(String name);
+}
