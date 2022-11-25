4bb3b78b8900241f8088ce2ba6c6b50e72a18091
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-11-25 11:07:43 +0100

    FIX: test cases for security purpose

--- api/src/test/java/com/codecool/quokka/controller/AccountControllerTest.java
+++ api/src/test/java/com/codecool/quokka/controller/AccountControllerTest.java
@@ -3,9 +3,7 @@ package com.codecool.quokka.controller;
 import com.codecool.quokka.QuokkaApplication;
 import com.codecool.quokka.model.account.Account;
 import com.codecool.quokka.model.account.AccountDto;
-import com.codecool.quokka.model.role.AccountRole;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.Maps;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -31,18 +28,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(
-        
         webEnvironment = SpringBootTest.WebEnvironment.MOCK,
         classes = QuokkaApplication.class)
 @AutoConfigureMockMvc
 public class AccountControllerTest {
     private AccountDto accountDto;
-    private AccountRole role = new AccountRole("Trader", "Trading");
     private Account account;
     @Autowired
     private MockMvc mvc;
 
     private String jwtHeader;
+
 
     @Before
     public void setUp() throws Exception {
@@ -50,15 +46,14 @@ public class AccountControllerTest {
         MvcResult response = mvc.perform(post("/api/v1/user")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(account)))
+                .andExpect(status().isOk())
                 .andReturn();
         String actualJson = response.getResponse().getContentAsString();
-
         accountDto = new ObjectMapper().readValue(actualJson, AccountDto.class);
-
         Map<String, String> credentials = Map.of("user_name", account.getUserName(), "password", account.getPassword());
-        response= mvc.perform(post("/api/v1/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(credentials)))
+        response = mvc.perform(post("/api/v1/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(credentials)))
                 .andExpect(status().isOk())
                 .andReturn();
         jwtHeader = response.getResponse().getHeader("Authorization");
@@ -66,13 +61,12 @@ public class AccountControllerTest {
 
     @After
     public void cleanUp() throws Exception {
-        HashMap data = new HashMap<>();
-        data.put("id", accountDto.getId());
+        Map<String, UUID> data = Map.of("id", accountDto.getId());
         mvc.perform(delete("/api/v1/user/")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(data))
-                        .header("Authorization", jwtHeader))
-                .andExpect(status().isOk());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(data))
+                .header("Authorization", jwtHeader));
+//                .andExpect(status().isOk());
     }
 
     @Test
@@ -85,20 +79,46 @@ public class AccountControllerTest {
                 .andReturn();
         String actualJson = response.getResponse().getContentAsString();
         assertTrue(actualJson.contains(accountDto.getId().toString()));
+    }
 
-
+    @Test
+    public void testGetUsersEndPointWithoutToken()
+            throws Exception {
+        mvc.perform(get("/api/v1/user"))
+                .andExpect(status().is(403));
     }
 
     @Test
     public void testDeleteUserById() throws Exception {
-        HashMap data = new HashMap<>();
-        data.put("id", accountDto.getId());
+        Map<String, UUID> data = Map.of("id", accountDto.getId());
         mvc.perform(delete("/api/v1/user").contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(data))
-                .header("Authorization", jwtHeader));
+                        .content(new ObjectMapper().writeValueAsString(data))
+                        .header("Authorization", jwtHeader))
+                .andExpect(status().isOk());
         mvc.perform(get("/api/v1/user/" + accountDto.getId().toString())
                         .header("Authorization", jwtHeader))
                 .andExpect(status().is(404));
+    }
+
+    @Test
+    public void testDeleteUserByIdWithWrongId() throws Exception {
+        String randomUUID = "c328a57e-9f3e-45bd-ab8d-11782a9885e0";
+        Map<String, String> data = Map.of("id", randomUUID);
+        mvc.perform(delete("/api/v1/user").contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(data))
+                        .header("Authorization", jwtHeader))
+                .andExpect(status().is(404));
+        mvc.perform(get("/api/v1/user/" + accountDto.getId().toString())
+                        .header("Authorization", jwtHeader))
+                .andExpect(status().is(200));
+    }
+
+    @Test
+    public void testDeleteUserByIdWithoutToken() throws Exception {
+        Map<String, UUID> data = Map.of("id", accountDto.getId());
+        mvc.perform(delete("/api/v1/user").contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(data)))
+                .andExpect(status().is(403));
     }
 
     @Test
@@ -127,6 +147,29 @@ public class AccountControllerTest {
     }
 
     @Test
+    public void testUpdateUserByIdWithoutToken() throws Exception {
+        String newUserName = "User3456";
+        Map<String, String> data = Map.of("fullName", newUserName);
+        mvc.perform(put("/api/v1/user/" + accountDto.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(data)))
+                .andExpect(status().is(403));
+    }
+
+    @Test
+    public void testUpdateUserByIdWithWrongId() throws Exception {
+        String newUserName = "User3456";
+        Map<String, String> data = Map.of("fullName", newUserName);
+        String randomUUID = "c328a57e-9f3e-45bd-ab8d-11782a9885e0";
+
+        mvc.perform(put("/api/v1/user/" + randomUUID)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", jwtHeader)
+                        .content(new ObjectMapper().writeValueAsString(data)))
+                .andExpect(status().is(404));
+    }
+
+    @Test
     public void testCreateNewUser() throws Exception {
         String userName = "UserAdded22";
         String fullName = "User Added22";
@@ -136,8 +179,8 @@ public class AccountControllerTest {
         MvcResult response = mvc.perform(post("/api/v1/user")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(account)))
-                        .andExpect(status().isOk())
-                        .andReturn();
+                .andExpect(status().isOk())
+                .andReturn();
         AccountDto createdAccount;
         String actualJson = response.getResponse().getContentAsString();
         createdAccount = new ObjectMapper().readValue(actualJson, AccountDto.class);
@@ -152,8 +195,8 @@ public class AccountControllerTest {
         String password = "asd";
         Account account = new Account(fullName, emailAddress, userName, password);
         mvc.perform(post("/api/v1/user")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(account)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(account)))
                 .andExpect(status().is4xxClientError());
     }
 
