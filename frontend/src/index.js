c21aef4688072faf968d8f7dc973fcee84f6d883
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-12-07 10:18:51 +0100

    ADD: authprovider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from "react-router-dom";
+import { AuthProvider } from './context/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <AuthProvider>
+                <App/>
+            </AuthProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
