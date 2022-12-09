99fcff22aa80154a1209ac25c3868d0bb0a2f1ab
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-12-09 21:07:50 +0100

    refactor stream client class

--- assetcache/src/stream.py
+++ assetcache/src/stream.py
@@ -1,22 +1,17 @@
 
+from typing import List, Callable, Awaitable
 from src.storages import DotEnvConfig, PrimitiveJsonDB
 from alpaca_trade_api import Stream
 
 
 class StreamClient:
 
-    def __init__(self, db: PrimitiveJsonDB, config: DotEnvConfig):
+    def __init__(self, stocks: List[str], on_stock_trade: Callable, cryptos: List[str], on_crypto_trade: Callable, config: DotEnvConfig):
         self._stream = Stream(key_id=config['APCA_API_KEY_ID'],
                               secret_key=config['APCA_API_SECRET_KEY'],
                               data_stream_url=config['APCA_API_STREAM_URL'],
+                              raw_data=True,
                               data_feed='iex')
-        stocks = db['assets']['stock']
-        cryptos = db['assets']['crypto']
-        self._stream.subscribe_trades(self.on_trade, *stocks)
-        self._stream.subscribe_crypto_trades(self.on_trade, *cryptos)
+        self._stream.subscribe_trades(on_stock_trade, *stocks)
+        self._stream.subscribe_crypto_trades(on_crypto_trade, *cryptos)
         self._stream.run()
-
-    async def on_trade(self, b):
-        print(b)
-        # TODO: implement storage into cache
-        pass
