eb06f9a99b550eae7806a417c893cbbb940a4f22
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-12-16 12:07:25 +0100

    FEAT: metrics in auth controller

--- api/src/main/java/com/codecool/quokka/controller/AuthController.java
+++ api/src/main/java/com/codecool/quokka/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.codecool.quokka.controller;
 
+import io.prometheus.client.Counter;
+import io.prometheus.client.Histogram;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class AuthController {
 
+    private static final Counter login_request = Counter.build().namespace("quokka").subsystem("api")
+            .name("login_request")
+            .help("total number of login operations").register();
+
     @PostMapping(path = "login")
-    public void login(){
-
+    public void login() {
+        login_request.inc();
     }
-
 }
