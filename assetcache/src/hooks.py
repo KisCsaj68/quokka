e781cb92b40f727a65fea1a485bdc70473fd4810
Zsolnai Viki <zsolnai.viki@gmail.com>
2024-10-27 11:02:43 +0100

    fix: oms host from env

--- assetcache/src/hooks.py
+++ assetcache/src/hooks.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from requests.adapters import TimeoutSauce
+from env import conf 
 
 REQUESTS_TIMEOUT_SECONDS = float(os.getenv("REQUESTS_TIMEOUT_SECONDS", 1))
 
@@ -18,7 +19,7 @@ def post_worker_init(worker):
     # Set it globally, instead of specifying ``timeout=..`` kwarg on each call.
 #     requests.adapters.TimeoutSauce = CustomTimeout
     worker.log.info('greetings from post worker init hook')
-    res = requests.post("http://oms:9000/api/v1/internal/open-orders")
+    res = requests.post(f'http://{conf["OMS_HOST"]}/api/v1/internal/open-orders')
     worker.log.info(res)
 
 
